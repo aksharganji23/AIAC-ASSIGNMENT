dba0e142bc11bfab8bbd30922a409d3389427ba2
aksharganji23 <2403A51399@sru.edu.in>
2025-09-12 16:41:51 +0530

    Add files via upload

---
+++ 2403A51399_Lab_Assignment_9.5.py
@@ -0,0 +1,32 @@
+def add_book(title, author, year):
+    """
+    Adds a new book to the library collection.
+
+    Args:
+        title (str): The title of the book.
+        author (str): The author of the book.
+        year (int): The publication year of the book.
+
+    Returns:
+        None
+
+    Description:
+        This function registers a new book in the library system using the provided title, author, and publication year.
+    """
+
+def issue_book(book_id, user_id):
+    """
+    Issues a book to a library user.
+
+    Args:
+        book_id (int): The unique identifier of the book to be issued.
+        user_id (int): The unique identifier of the user borrowing the book.
+
+    Returns:
+        None
+
+    Description:
+        This function records the issuance of a book to a user, updating the library system accordingly.
+    """
+
+
